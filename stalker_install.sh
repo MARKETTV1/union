03bb70f0af4d6b77402e64e3f006e54bf9f6a601
MARKETTV1 <aliridakarim8@gmail.com>
2025-09-09 03:01:19 +0100

    Create stalker_install.sh

---
+++ stalker_install.sh
@@ -0,0 +1,3 @@
+
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/Uni_Stalker/Servers/Stalker.json https://raw.githubusercontent.com/MARKETTV1/union/refs/heads/main/Stalker.json && killall -9 enigma2
